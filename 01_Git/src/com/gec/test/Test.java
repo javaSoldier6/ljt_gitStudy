0487f0e65e4d56e0c0f267e6b73caa043e151794
ljt <2690677125@qq.com>
2020-08-14 16:19:52 +0800

    我加了一句话提交到分支

--- 01_Git/src/com/gec/test/Test.java
+++ 01_Git/src/com/gec/test/Test.java
@@ -1,9 +1,11 @@
 package com.gec.test;
 
 public class Test {
+	
 	public static void main(String[] args) {
 		System.out.println("my frist git!!!");
 		System.out.println("我搞了");
 		System.out.println("我创建了01_Git_branch分支");
+		System.out.println("合并分支");
 	}
 }
